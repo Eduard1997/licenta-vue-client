bfc1300df3aaf367639c445793f74adfc49e0229
g.eduard <g.eduard@leadsoft.ro>
2020-04-13 12:12:38 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import axios from 'axios';
 
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 new Vue({
   router,
