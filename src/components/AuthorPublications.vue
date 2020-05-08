33a8b8ab98a5b2216a31c029b7270c61865f4ff8
g.eduard <g.eduard@leadsoft.ro>
2020-05-08 18:27:49 +0300

    edit and delete
    functionality and design

--- src/components/AuthorPublications.vue
+++ src/components/AuthorPublications.vue
@@ -4,7 +4,7 @@
     <div class="alert alert-danger" v-if="showAlert">{{alertText}}</div>
     <div class="row">
       <div class="col">
-        <table class="table table-bordered table-responsive-xl" v-if="authorPublications">
+        <table class="table table-bordered table-responsive-xl table-hover" v-if="authorPublications">
           <thead class="thead-light">
             <tr>
               <th class="text-center" >Name</th>
@@ -21,13 +21,14 @@
               <th class="text-center" style="white-space: nowrap">DBLP <br> - Type -</th>
               <th class="text-center" style="white-space: nowrap">DBLP <br> - Venue -</th>
               <th class="text-center" style="white-space: nowrap">DBLP <br> - Link -</th>
+              <th class="text-center">Actions</th>
             </tr>
           </thead>
           <tbody>
             <template v-for="publication in authorPublications">
               <tr>
                 <td>
-                  <a href="javascript:;" v-on:click="getCitations(publication.title)">{{publication.title}}</a>
+                  <a href="javascript:;" v-on:click="getCitations(publication.title)" v-b-tooltip.hover title="View document citations">{{publication.title}}</a>
                 </td>
                 <td class="text-center"> <span v-if="publication.year">{{publication.year}}</span> <span v-else>-</span> </td>
                 <td class="text-center"> <span v-if="typeof(publication.cited_by_scholar) !== 'undefined' && publication.cited_by_scholar">{{publication.cited_by_scholar}}</span> <span v-else>-</span> </td>
@@ -51,6 +52,10 @@
                   <span v-else>-</span>
                 </td>
                 <td class="text-center"> <span class="badge badge-info" v-if="typeof(publication.dblp_link) !== 'undefined' && publication.dblp_link" @click="openUrl(publication.dblp_link)" style="cursor: pointer">View document</span> <span v-else>-</span> </td>
+                <td align="center">
+                  <i class="fa fa-trash delete-icon" v-on:click="deletePublication(publication.title)" v-b-tooltip.hover title="Delete document"></i>
+                  <i class="fa fa-edit edit-icon ml-2" v-on:click="editPublication(publication.title)" v-b-tooltip.hover title="Edit document"></i>
+                </td>
               </tr>
             </template>
           </tbody>
@@ -65,6 +70,56 @@
 
         </div>
       </b-modal>
+    <b-modal id="edit-publication-modal" size="xl" :title="editModalTitle" :ok-title="'Save'" ok-variant="success" cancel-variant="danger" @ok="saveEditData()">
+      <b-input-group>
+        <template v-slot:prepend>
+          <b-input-group-text class="edit-input-text">Title</b-input-group-text>
+        </template>
+        <b-form-input v-model="editTitle"></b-form-input>
+      </b-input-group>
+
+      <b-input-group class="mt-2">
+        <template v-slot:prepend>
+          <b-input-group-text class="edit-input-text">Publication</b-input-group-text>
+        </template>
+        <b-form-input v-model="editPublicationText"></b-form-input>
+      </b-input-group>
+
+      <b-input-group class="mt-2">
+        <template v-slot:prepend>
+          <b-input-group-text class="edit-input-text">URL</b-input-group-text>
+        </template>
+        <b-form-input v-model="editModalUrl"></b-form-input>
+      </b-input-group>
+
+      <b-input-group class="mt-2">
+        <template v-slot:prepend>
+          <b-input-group-text class="edit-input-text">Scopus Aggregation Type</b-input-group-text>
+        </template>
+        <b-form-input v-model="editScopusAggregation"></b-form-input>
+      </b-input-group>
+
+      <b-input-group class="mt-2">
+        <template v-slot:prepend>
+          <b-input-group-text class="edit-input-text">Scopus Subtype Description</b-input-group-text>
+        </template>
+        <b-form-input v-model="editScopusDescription"></b-form-input>
+      </b-input-group>
+
+      <b-input-group class="mt-2">
+        <template v-slot:prepend>
+          <b-input-group-text class="edit-input-text">DBLP Type</b-input-group-text>
+        </template>
+        <b-form-input v-model="editDBLPType"></b-form-input>
+      </b-input-group>
+
+      <b-input-group class="mt-2">
+        <template v-slot:prepend>
+          <b-input-group-text class="edit-input-text">DBLP Venue</b-input-group-text>
+        </template>
+        <b-form-input v-model="editDBLPVenue"></b-form-input>
+      </b-input-group>
+    </b-modal>
   </div>
 </template>
 
@@ -74,7 +129,7 @@
   import $ from 'jquery';
 
   export default {
-    components: { PulseLoader },
+    components: { PulseLoader},
     props: {'authorName': String},
     name: 'AuthorPubications',
     data() {
@@ -86,6 +141,14 @@
         authorPublications: '',
         authorName: '',
         modalTitle: '',
+        editModalTitle: '',
+        editTitle: '',
+        editPublicationText: '',
+        editModalUrl: '',
+        editScopusAggregation:'',
+        editScopusDescription:'',
+        editDBLPType: '',
+        editDBLPVenue: '',
         citationsArray: {}
       };
     },
@@ -145,6 +208,76 @@
             $('.modal-content-citations').append(alertHtml);
           }
         }
+      },
+      deletePublication(publicationName) {
+        this.$swal({
+          title: 'Are you sure?',
+          text: publicationName,
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+          if(result.value === true) {
+            this.confirmDeletion(publicationName);
+          }
+        });
+      },
+      confirmDeletion(publicationName) {
+        delete this.authorPublications[publicationName];
+        this.$forceUpdate();
+      },
+      editPublication(publicationName) {
+        this.editModalTitle = publicationName;
+        this.editTitle = publicationName;
+        this.editPublicationText = this.authorPublications[publicationName]['publication_name'];
+        this.editModalUrl = this.authorPublications[publicationName]['url'];
+        this.editScopusAggregation = this.authorPublications[publicationName]['aggregation_type_scopus'] !== 'undefined' ? this.authorPublications[publicationName]['aggregation_type_scopus'] : '-';
+        this.editScopusDescription = typeof this.authorPublications[publicationName]['subtype_description_scopus'] !== 'undefined' ? this.authorPublications[publicationName]['subtype_description_scopus'] : '-';
+        this.editDBLPType = typeof this.authorPublications[publicationName]['dblp_type'] !== 'undefined' ? this.authorPublications[publicationName]['dblp_type'] : '-';
+        this.editDBLPVenue = typeof this.authorPublications[publicationName]['dblp_venue'] !== 'undefined' ? this.authorPublications[publicationName]['dblp_venue'] : '-';
+        this.$root.$emit("bv::show::modal", "edit-publication-modal");
+      },
+      saveEditData() {
+        let newPublicationName = this.editTitle.charAt(0).toUpperCase() + this.editTitle.slice(1);
+        let oldPublicationName = this.editModalTitle;
+        this.authorPublications[oldPublicationName]['title'] = newPublicationName;
+        this.authorPublications[oldPublicationName]['publication_name'] = this.editPublicationText;
+        this.authorPublications[oldPublicationName]['url'] = this.editModalUrl;
+        this.authorPublications[oldPublicationName]['aggregation_type_scopus'] = this.editScopusAggregation;
+        this.authorPublications[oldPublicationName]['subtype_description_scopus'] = this.editScopusDescription;
+        this.authorPublications[oldPublicationName]['dblp_type'] = this.editDBLPType;
+        this.authorPublications[oldPublicationName]['dblp_venue'] = this.editDBLPVenue;
+        Object.defineProperty(this.authorPublications, newPublicationName, Object.getOwnPropertyDescriptor(this.authorPublications, oldPublicationName));
+        delete this.authorPublications[oldPublicationName];
+        this.authorPublications = this.orderKeys(this.authorPublications);
+        var index = Object.keys(this.authorPublications).indexOf(newPublicationName);
+        $('tr').eq(index+1).addClass('table-success');
+        $('tr').eq(index+1).attr('id', 'scroll-to');
+        window.location.href = '#scroll-to';
+        setTimeout(function() {
+          $('tr').eq(index+1).removeClass('table-success');
+          $('tr').eq(index+1).removeAttr('id');
+        }, 3000);
+      },
+      orderKeys(obj) {
+        var keys = Object.keys(obj).sort(function keyOrder(k1, k2) {
+          if (k1 < k2) return -1;
+          else if (k1 > k2) return +1;
+          else return 0;
+        });
+
+        var i, after = {};
+        for (i = 0; i < keys.length; i++) {
+          after[keys[i]] = obj[keys[i]];
+          delete obj[keys[i]];
+        }
+
+        for (i = 0; i < keys.length; i++) {
+          obj[keys[i]] = after[keys[i]];
+        }
+        return obj;
       }
     },
     created() {
@@ -166,6 +299,22 @@
   }
   table {
     font-size: 13px;
+  }
+  .delete-icon {
+    color: red;
+    font-size: 21px;
+    cursor: pointer;
+  }
+  .edit-icon {
+    color: dodgerblue;
+    font-size: 21px;
+    cursor: pointer;
+  }
+  .edit-input-text {
+    font-weight: 600;
+  }
+  .success-border {
+    box-shadow: 0 0 0 3px green;
   }
 </style>
 
--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 import axios from 'axios';
 
 Vue.use(BootstrapVue);
+Vue.use(VueSweetalert2);
 
 Vue.config.productionTip = false;
 // axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
